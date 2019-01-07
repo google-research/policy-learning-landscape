546222211cceb14d2c9ad83936106ea96c120199
zafarali <zafarali.ahmed@gmail.com>
2019-01-07 11:06:20 +0530

    Add experiment utilities.

---
+++ method_validation_experiments/utils.py
@@ -0,0 +1,69 @@
+"""Utilities for simple training."""
+import numpy as np
+import tensorflow as tf
+
+
+def load_data(batch_size=64):
+  """Load fashion MNIST and required variables."""
+  ((X_train, Y_train),
+   (X_test, Y_test)) = tf.keras.datasets.fashion_mnist.load_data()
+
+  dataset_size = len(X_train)
+  output_size = 10
+
+  # Convert the array to float32 and normalize by 255
+  # Add a dim to represent the RGB index for CNNs
+  X_train = np.expand_dims((X_train.astype(np.float32) / 255.0), -1)
+  X_test = np.expand_dims((X_test.astype(np.float32) / 255.0), -1)
+  image_size = X_train.shape[1:]
+
+  Y_train = (
+          tf.keras.utils.to_categorical(
+              Y_train, output_size).astype(np.float32))
+  Y_test = (
+          tf.keras.utils.to_categorical(
+              Y_test, output_size).astype(np.float32))
+
+  # FULL_DATASET_SIZE = X_train.shape[0]
+
+  # original_dataset = tf.data.Dataset.from_tensor_slices((X_train, Y_train))
+  # dataset = original_dataset.batch(batch_size)
+
+  # dataset = dataset.prefetch(1)
+  # dataset = dataset.repeat()
+
+  # full_dataset = original_dataset.batch(FULL_DATASET_SIZE)
+  return ((X_train, Y_train),
+          (X_test, Y_test),
+          {'image_size': image_size,
+           'output_size': output_size})
+
+def get_model(shapes, hidden_units=0, device='gpu:0', hidden_activation=None):
+  with tf.device(device):
+    layers = []
+    layers.append(tf.keras.layers.Flatten(input_shape=shapes['input_size']))
+    if hidden_units > 0:
+      layers.append(tf.keras.layers.Dense(
+          hidden_units, activation=hidden_activation))
+    layers.append(tf.keras.layers.Dense(
+        shapes['output_size'], activation=tf.nn.softmax))
+
+    return tf.keras.Sequential(layers)
+
+def get_callbacks(hidden_units=0):
+  callbacks = [
+    tf.keras.callbacks.ModelCheckpoint(
+        ('./checkpoints-' + str(hidden_units) + \
+         '/{epoch:02d}-{val_loss:.2f}.weights'),
+        save_weights_only=True,
+        verbose=1,
+        )
+  ]
+  return callbacks
+
+
+if __name__ == '__main__':
+  load_data()
+  get_model({'input_size':(784, 1), 'output_size': 10})
+  get_callbacks()
+
