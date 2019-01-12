b6938405c4d6d729d615ded7b3ef22eef3b1b7a4
zafarali <zafarali.ahmed@gmail.com>
2019-01-12 17:46:52 -0500

    Add bash script to run.

---
+++ method_validation_experiments/run_method_analysis.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1              # Number of GPUs (per node)
+#SBATCH --mem=16384M               # memory (per node)
+#SBATCH --time=0-10:00            # time (DD-HH:MM)
+#SBATCH --n-tasks=1
+#SBATCH --cpus-per-task=6
+#SBATCH --array=0
+#SBATCH --job-name=meth_fMNISTbasic
+#SBATCH --error=./meth_fMNISTbasic_%j.err
+#SBATCH --output=./meth_fMNISTbasic_%j.out
+
+[ -z "$WEIGHTS" ] && echo "Please provide WEIGHTS" && exit 1;
+module load cuda/8.0.44
+module load cudnn/7.0
+source $BRAIN_ENV
+
+python3 analyze_method.py --device "gpu:0" \
+--save_directory $SCRATCH/basic_training --load_directory $WEIGHTS --step_size 0.01 --n_samples 5000
+
+python3 analyze_method.py --device "gpu:0" \
+--save_directory $SCRATCH/basic_training --load_directory $WEIGHTS --step_size 0.1 --n_samples 5000
+
+python3 analyze_method.py --device "gpu:0" \
+--save_directory $SCRATCH/basic_training --load_directory $WEIGHTS --step_size 0.5 --n_samples 5000
+
+python3 analyze_method.py --device "gpu:0" \
+--save_directory $SCRATCH/basic_training --load_directory $WEIGHTS --step_size 1.0 --n_samples 5000
+
+python3 analyze_method.py --device "gpu:0" \
+--save_directory $SCRATCH/basic_training --load_directory $WEIGHTS --step_size 2.0 --n_samples 5000
