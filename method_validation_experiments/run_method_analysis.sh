da9439ddba65dc5095cb8ca661823668b804a3df
zafarali <zafarali.ahmed@gmail.com>
2019-01-13 15:49:09 -0500

    Use more array.

--- method_validation_experiments/run_method_analysis.sh
+++ method_validation_experiments/run_method_analysis.sh
@@ -3,8 +3,8 @@
 #SBATCH --mem=16384M               # memory (per node)
 #SBATCH --time=0-10:00            # time (DD-HH:MM)
 #SBATCH --n-tasks=1
-#SBATCH --cpus-per-task=6
-#SBATCH --array=0
+#SBATCH --cpus-per-task=2
+#SBATCH --array=0-3
 #SBATCH --job-name=meth_fMNISTbasic
 #SBATCH --error=./meth_fMNISTbasic_%j.err
 #SBATCH --output=./meth_fMNISTbasic_%j.out
@@ -15,16 +15,14 @@ module load cudnn/7.0
 source $BRAIN_ENV
 
 python3 analyze_method.py --device "gpu:0" \
---save_directory $SCRATCH/basic_training --load_directory $WEIGHTS --step_size 0.01 --n_samples 5000
-
+--save_directory $SCRATCH/basic_training --load_directory $WEIGHTS --step_size 0.01 --n_samples 1000
+echo "done 0.01"
 python3 analyze_method.py --device "gpu:0" \
---save_directory $SCRATCH/basic_training --load_directory $WEIGHTS --step_size 0.1 --n_samples 5000
-
+--save_directory $SCRATCH/basic_training --load_directory $WEIGHTS --step_size 0.1 --n_samples 1000
+echo "done 0.1"
 python3 analyze_method.py --device "gpu:0" \
---save_directory $SCRATCH/basic_training --load_directory $WEIGHTS --step_size 0.5 --n_samples 5000
-
+--save_directory $SCRATCH/basic_training --load_directory $WEIGHTS --step_size 0.5 --n_samples 1000
+echo "done 0.5"
 python3 analyze_method.py --device "gpu:0" \
---save_directory $SCRATCH/basic_training --load_directory $WEIGHTS --step_size 1.0 --n_samples 5000
-
-python3 analyze_method.py --device "gpu:0" \
---save_directory $SCRATCH/basic_training --load_directory $WEIGHTS --step_size 2.0 --n_samples 5000
+--save_directory $SCRATCH/basic_training --load_directory $WEIGHTS --step_size 1.0 --n_samples 1000
+echo "done 1.0"
